9e3cd4edb8748f7c3ef10122fc7abccf4c889468
Bhavyashree-DC <bhavyadc05@gmail.com>
2023-10-18 18:09:10 +0530

    Added home-section footer with linear gradient effect

--- script.js
+++ script.js
@@ -1,42 +1,52 @@
+document.addEventListener('DOMContentLoaded', function () {
+  let currentSlider = 1;
 
-let currentSlider = 1;
+  function toggleSlider(sliderNumber) {
+      // Hide all sliders
+      const sliders = document.querySelectorAll('.slider-container');
+      sliders.forEach(slider => {
+          slider.style.display = 'none';
+      });
 
-function toggleSlider() {
-    // Hide all sliders
-    const sliders = document.querySelectorAll('.slider-container');
-    sliders.forEach(slider => {
-        slider.style.display = 'none';
-    });
+      // Show the clicked slider
+      currentSlider = sliderNumber;
+      const current = document.getElementById(`slider${currentSlider}`);
+      current.style.display = 'block';
 
-    // Show the current slider
-    const current = document.getElementById(`slider${currentSlider}`);
-    current.style.display = 'block';
+      // Get the background color of the corresponding down_arrow_outer
+      const downArrowOuter = document.querySelector(`#slider${currentSlider} .down_arrow_outer`);
+      const bgColor = window.getComputedStyle(downArrowOuter).backgroundColor;
 
-    // Get the background color of the corresponding down_arrow_outer
-    const downArrowOuter = document.querySelector(`#slider${currentSlider} .down_arrow_outer`);
-    const bgColor = window.getComputedStyle(downArrowOuter).backgroundColor;
+      // Update the dot button background color
+      const dotButtons = document.querySelectorAll('.dots-list button');
+      dotButtons.forEach((dotButton, index) => {
+          dotButton.style.backgroundColor = index + 1 === currentSlider ? bgColor : '';
+      });
+  }
 
-    // Update the dot button background color
-    const dotButtons = document.querySelectorAll('.dots-list button');
-    dotButtons.forEach((dotButton, index) => {
-        dotButton.style.backgroundColor = index + 1 === currentSlider ? bgColor : '';
-    });
+  // Add event listeners to your buttons
+  const buttons = document.querySelectorAll('.dots-list button');
+  buttons.forEach((button, index) => {
+      button.addEventListener('click', () => {
+          toggleSlider(index + 1); // Index starts from 0
+      });
+  });
 
-    currentSlider = currentSlider < 4 ? currentSlider + 1 : 1;
-}
+  // Automatically start with the first slider
+  toggleSlider(currentSlider);
 
-toggleSlider();
-
-setInterval(toggleSlider, 8000);
-
+  // Add an interval to automatically switch sliders
+  setInterval(function () {
+      currentSlider = currentSlider < 3 ? currentSlider + 1 : 1;
+      toggleSlider(currentSlider);
+  }, 8000);
+});
 
 
 // down  arrow scrolling functionality
 
-// Get all arrow down icons with the common class
 const arrowDownIcons = document.querySelectorAll(".down_arrow_outer i");
 
-// Add click event listeners to scroll to the corresponding home section
 arrowDownIcons.forEach((icon, index) => {
     icon.addEventListener("click", function () {
         const searchIdeaContainer = document.getElementById(`sliderDownArrow${index + 1}`);
