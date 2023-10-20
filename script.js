649df2710bb87175f211494e623660575d3f4d08
Bhavyashree-DC <bhavyadc05@gmail.com>
2023-10-20 17:49:19 +0530

    Added image, text transition

--- script.js
+++ script.js
@@ -1,69 +1,75 @@
 document.addEventListener('DOMContentLoaded', function () {
-  let currentSlider = 1;
+    let currentSlider = 1;
+    const totalSlides = 4; // Change this to the total number of slides
 
-  function toggleSlider(sliderNumber) {
-      // Hide all sliders
-      const sliders = document.querySelectorAll('.slider-container');
-      sliders.forEach(slider => {
-          slider.style.display = 'none';
-      });
+    function toggleSlider(sliderNumber) {
+        // Hide all sliders
+        const sliders = document.querySelectorAll('.slider-container');
+        sliders.forEach(slider => {
+            slider.style.display = 'none';
+        });
 
-      // Show the clicked slider
-      currentSlider = sliderNumber;
-      const current = document.getElementById(`slider${currentSlider}`);
-      current.style.display = 'block';
+        // Show the clicked slider
+        currentSlider = sliderNumber;
+        const current = document.getElementById(`slider${currentSlider}`);
+        current.style.display = 'block';
 
-      // Get the background color of the corresponding down_arrow_outer
-      const downArrowOuter = document.querySelector(`#slider${currentSlider} .down_arrow_outer`);
-      const bgColor = window.getComputedStyle(downArrowOuter).backgroundColor;
+        // Get the background color of the corresponding down_arrow_outer
+        const downArrowOuter = document.querySelector(`#slider${currentSlider} .down_arrow_outer`);
+        const bgColor = window.getComputedStyle(downArrowOuter).backgroundColor;
 
-      // Update the dot button background color
-      const dotButtons = document.querySelectorAll('.dots-list button');
-      dotButtons.forEach((dotButton, index) => {
-          dotButton.style.backgroundColor = index + 1 === currentSlider ? bgColor : '';
-      });
-  }
+        // Update the dot button background color
+        const dotButtons = document.querySelectorAll('.dots-list button');
+        dotButtons.forEach((dotButton, index) => {
+            dotButton.style.backgroundColor = index + 1 === currentSlider ? bgColor : '';
+        });
+    }
 
-  // Add event listeners to your buttons
-  const buttons = document.querySelectorAll('.dots-list button');
-  buttons.forEach((button, index) => {
-      button.addEventListener('click', () => {
-          toggleSlider(index + 1); // Index starts from 0
-      });
-  });
+    // Add event listeners to your buttons
+    const buttons = document.querySelectorAll('.dots-list button');
+    buttons.forEach((button, index) => {
+        button.addEventListener('click', () => {
+            toggleSlider(index + 1); 
+            clearInterval(autoSlideInterval); 
+            autoSlide(); // Restart the automatic slideshow
+        });
+    });
 
-  // Automatically start with the first slider
-  toggleSlider(currentSlider);
+    // Automatically start with the first slider
+    toggleSlider(currentSlider);
 
-  // Add an interval to automatically switch sliders
-  setInterval(function () {
-      currentSlider = currentSlider < 3 ? currentSlider + 1 : 1;
-      toggleSlider(currentSlider);
-  }, 8000);
-});
+    
+    const autoSlideIntervalTime = 6000; 
+    let autoSlideInterval;
+
+    function autoSlide() {
+        autoSlideInterval = setInterval(function () {
+            currentSlider = currentSlider < totalSlides ? currentSlider + 1 : 1;
+            toggleSlider(currentSlider);
+        }, autoSlideIntervalTime);
+    }
 
 
-// down  arrow scrolling functionality
+    autoSlide();
 
-const arrowDownIcons = document.querySelectorAll(".down_arrow_outer i");
+    const downArrowIcons = document.querySelectorAll('.down_arrow_icon');
 
-arrowDownIcons.forEach((icon, index) => {
-    icon.addEventListener("click", function () {
-        const searchIdeaContainer = document.getElementById(`sliderDownArrow${index + 1}`);
-        searchIdeaContainer.scrollIntoView({ behavior: "smooth" });
+    downArrowIcons.forEach(function (icon) {
+        icon.addEventListener('click', function () {
+            const sectionId = icon.getAttribute('data-section');
+            const section = document.getElementById(sectionId);
+            if (section) {
+                section.scrollIntoView({ behavior: 'smooth' });
+            }
+        });
+    });
+
+    // scroll up functionality
+    const scrollToHome = document.getElementById("scrollToHome");
+    const homeSection = document.querySelector(".home_section");
+
+    scrollToHome.addEventListener("click", function () {
+        homeSection.scrollIntoView({ behavior: "smooth" });
     });
 });
-
-
-// scroll up functionality
-
-const scrollToHome = document.getElementById("scrollToHome");
-const homeSection = document.querySelector(".home_section");
-
-scrollToHome.addEventListener("click", function () {
-    homeSection.scrollIntoView({ behavior: "smooth" });
-});
-
-
-
 
